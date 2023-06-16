bd2caa32df7cf9e539b89cd893ba9b530a2e1a21
RafaelAdroit <rafael@adroitoverseas.com>
2023-06-16 13:37:29 -0700

    changes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Modal.css'
 
 const Modal = ({ data, isOpen, onClose }) => {
+
+    const timeRanges = ['20:00', '21:00', '22:00', '23:00', '24:00', '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
     if (!isOpen) return null;
 
     return (
@@ -14,12 +16,22 @@ const Modal = ({ data, isOpen, onClose }) => {
                     <h2>Título del modal</h2>
                     <p>Contenido del modal.</p>
                 </div>
-                <div className="modal-content">
-                    <div className='channels-name'>
-                        {data.map((channel) => { return <div className='channel-row'><img className='image' src={channel.image} /><span>{channel.name}</span></div> })}
-                    </div>
-                    <div className='schedule'>
-                        horarios div
+                <div className='modal-lower-content'>
+                    <div className="modal-content">
+                        <div className='channels-name'>
+                            <div className='channel-header'>
+                                <span>HOY</span>
+                            </div>
+                            {data.map((channel) => { return <div className='channel-row'><img className='image' alt='tv-logo' src={channel.image} /><span>{channel.name}</span></div> })}
+                        </div>
+                        <div className='schedule'>
+                            <div className='time-range-row'>
+                                {timeRanges.map((time) => { return <div className='time-row'>{time}</div> })}
+                            </div>
+                            <div className='programs-row'>
+                             {data.map((channel) => {return <div style={{display:'flex'}}>{channel.events.map((program) => { return <div className='program'>{program.name}</div>})}</div> })}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
